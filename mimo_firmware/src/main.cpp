857207271b7189e85ffe9a306b826d75c114b768
André Escorel <gustavo.escorel@gmail.com>
2021-09-22 18:57:40 -0300

    modem sleep funcional

--- mimo_firmware/src/main.cpp
+++ mimo_firmware/src/main.cpp
@@ -5,31 +5,51 @@
 #include "status_server.h"
 #include "connection.h"
 
-extern "C"
-{
 #include "user_interface.h"
 #include "gpio.h"
-}
 
-#define BUTTON_PIN D1 // PINO GPIO0
+#define BUTTON_PIN 0
 
-void get_pin_status_update_server(long time_delay = 4000)
+byte buttonState = 0;
+
+void get_pin_status_update_server(long time_delay = 2000)
 {
-  byte buttonState = digitalRead(BUTTON_PIN);
+  byte newButtonState = digitalRead(BUTTON_PIN);
+  if (buttonState == newButtonState)
+    return;
   delay(time_delay);
-  if (buttonState == digitalRead(BUTTON_PIN))
+  if (newButtonState == digitalRead(BUTTON_PIN))
   {
-    Serial.println("Enviando...");
-    Serial.print("Pin state: ");
-    Serial.println(buttonState);
-    if (update_status_on_server(buttonState, getDeviceName().substring(5)))
+    buttonState = newButtonState;
+    Serial.println("contact!");
+    if (WiFi.status() != WL_CONNECTED)
     {
-      Serial.println("Sucesso!");
+      WiFi.forceSleepWake();
+      delay(10);
+    }
+    if (wifi_reconnect())
+    {
+
+      Serial.println("Enviando...");
+      Serial.print("Pin state: ");
+      Serial.println(buttonState);
+      if (update_status_on_server(buttonState, getDeviceName().substring(6)))
+      {
+        Serial.println("Sucesso!");
+      }
+      else
+      {
+        Serial.println("Error!");
+      }
     }
     else
     {
-      Serial.println("Error!");
+      ESP.reset();
     }
+  }
+  else
+  {
+    Serial.print('.');
   }
 }
 
@@ -38,19 +58,27 @@ void setup()
   Serial.begin(115200);
   Serial.setTimeout(2000);
   pinMode(BUTTON_PIN, INPUT);
-  // gpio_init();
-  while (!Serial)
-    ;
+  gpio_init();
   Serial.flush();
   Serial.print('\n');
   wifi_connection_setup();
-  get_pin_status_update_server();
-  Serial.println("Indo dormir!");
-  Serial.flush();
-  ESP.deepSleep(1e7);
   Serial.flush();
 }
 
 void loop()
 {
+  get_pin_status_update_server();
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+    WiFi.mode(WIFI_OFF);
+    WiFi.forceSleepBegin();
+    Serial.println("WiFi is down");
+    delay(10);
+  }
+  else
+  {
+    Serial.print("Pin state: ");
+    Serial.println(digitalRead(BUTTON_PIN));
+    delay(300);
+  }
 }
