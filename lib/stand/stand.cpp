d08eaa4166952a66512342f2fb5d73bfe362fdb2
Andre Escorel <agver@ecomp.poli.br>
2022-02-09 22:31:12 -0300

    last try

---
+++ lib/stand/stand.cpp
@@ -0,0 +1,68 @@
+
+#include <stand.h>
+
+byte get_sensor_status(float time, byte last_status)
+{
+
+  byte read[] = {10, 10, 10};
+  read[0] = sensor();
+  log_value("[status]: sensor_read[0] = ", read[0]);
+
+  if (read[0] == last_status)
+    return read[0];
+
+  read[1] = get_debounced_status(time * (160 + ((read[0] == 0) ? 200 : 700)));
+  log_value("[status]: sensor_read[1] = ", read[1]);
+
+  if (read[0] != 0)
+  {
+    read[2] = get_debounced_status(time * 140);
+    log_value("[status]: sensor_read[2] = ", read[2]);
+    
+    bool __ = read[1] == read[0] && read[2] != read[0];
+    
+    return __ ? FALSE_ALARM_CODE : read[2];
+  }
+
+  return read[1];
+}
+
+byte get_debounced_status(float time)
+{
+  await(time);
+  return sensor();
+}
+
+bool update_server(byte code)
+{
+  WiFi.mode(WIFI_STA);
+  if (wifi_reconnect())
+  {
+    log("[main]: Conectado!");
+    log("[main]: Enviando...");
+    log_value("[main]: Sensor status: ", code);
+    if (update_status_on_server(code, getDeviceName().substring(6)))
+    {
+      log("Sucesso!");
+      return true;
+    }
+    else
+    {
+      log("Error!");
+      return false;
+    }
+  }
+  else
+  {
+    log("[WiFi]: Wifi indisponivel!");
+    return false;
+  }
+}
+
+void led_on() {
+  // GPIO_OUTPUT_SET(GPIO_ID_PIN(RESTORE_PIN), LOW);
+}
+
+void led_off() {
+  // GPIO_OUTPUT_SET(GPIO_ID_PIN(RESTORE_PIN), HIGH);
+}
---
+++ lib/stand/stand.h
@@ -0,0 +1,40 @@
+#ifndef __STAND_H
+#define __STAND_H
+
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h>
+#include "status_server.h"
+#include "connection.h"
+
+extern "C"
+{
+#include "gpio.h"
+#include "user_interface.h"
+}
+
+
+#define SENSOR_PIN 0
+#define RESTORE_PIN 2
+#define CLEAN_CODE 0
+#define ALARM_CODE 1
+#define FALSE_ALARM_CODE 3
+
+#ifdef DEBUG_LOG
+#define log_value(msg, data) (void)0
+#define log(msg) (void)0
+#else
+#define log(msg) (Serial.println(msg))
+#define log_value(msg, data) ((Serial.print((msg))), (log((data))), (void)0)
+#endif
+
+#define gpioRead(pin) (GPIO_INPUT_GET(GPIO_ID_PIN(pin)))
+#define sensor() (gpioRead(SENSOR_PIN))
+#define await(t) (delay(static_cast<long>(t)))
+
+byte get_sensor_status(float time, byte last_status);
+byte get_debounced_status(float time);
+bool update_server(byte code);
+void led_on();
+void led_off();
+
+#endif
